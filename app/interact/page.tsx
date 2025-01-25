d3e160354037a1f39cbd154511ed119e54969480
DJ <diwataangelito@gmail.com>
2025-01-25 17:38:52 +0000

    3D changes

--- app/interact/page.tsx
+++ app/interact/page.tsx
@@ -1,14 +1,12 @@
 // pages/stl-viewer.js
 import React from 'react';
-import DynamicSTLViewer from './components/DynamicSTLViewer';
-import DynamicGLTFViewer from './components/DynamicGLTFViewer';
+import DynamicGLTFViewer from '../components/DynamicGLTFViewer';
 
 export default function STLViewerPage() {
   const stlFileUrl = '/GHOST.stl'; // Replace with the path to your STL file
 
   return (
     <div>
-      <h1>STL 3D Viewer</h1>
       <DynamicGLTFViewer url={stlFileUrl} />
     </div>
   );
